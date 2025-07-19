cac3c5f76a4dec0402a6da6800c6fe1f88713d3a
kerzox <williampappin@gmail.com>
2025-07-19 22:10:54 +1000

    Initial

---
+++ dist/widget.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.testFunction = void 0;
+const testFunction = () => {
+    console.log('Widget has been loaded✨');
+};
+exports.testFunction = testFunction;
